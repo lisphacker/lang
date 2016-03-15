b6d2f53a8a1063c5038fa7baf2599301152e125a
Gautham Ganapathy <gautham@lisphacker.org>
2016-03-15 01:59:27 +0000

    Fixed white cube

--- python/gl/cube.py
+++ python/gl/cube.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 import numpy as np
+import math
 import sys
 
 from OpenGL.GL import *
@@ -52,7 +53,7 @@ class Matrix4x4(Base):
     def __init__(self, values, *args, **kwargs):
         super(Matrix4x4, self).__init__(*args, **kwargs)
 
-        self.values = values
+        self.values = values.squeeze().T
 
     def __mul__(self, o):
         return Matrix4x4(values=np.dot(self.values, o.values))
@@ -72,16 +73,45 @@ class Matrix4x4(Base):
                                           [0, 0, 1, z],
                                           [0, 0, 0, 1]], dtype=np.float32))
 
+    @staticmethod
+    def create_rotate_matrix(x, y, z):
+        cx = math.cos(x)
+        sx = math.sin(x)
+
+        cy = math.cos(y)
+        sy = math.sin(y)
+        
+        cz = math.cos(z)
+        sz = math.sin(z)
+                
+        xrot = np.array([[1,  0,  0,  0],
+                         [0, cx, -sx, 0],
+                         [0, sx,  cx, 0],
+                         [0,  0,  0,  1]], dtype=np.float32)
+        yrot = np.array([[ cy, 0, sy, 0],
+                         [  0, 1,  0, 0],
+                         [-sy, 0, cy, 0],
+                         [  0, 0,  0, 1]], dtype=np.float32)
+        zrot = np.array([[cz, -sz, 0, 0],
+                         [sz,  cz, 0, 0],
+                         [ 0,   0, 1, 0],
+                         [ 0,   0, 0, 1]], dtype=np.float32)
+        rot = np.dot(xrot, np.dot(yrot, zrot))
+
+        return Matrix4x4(values=rot, dtype=np.float32)
+
+
 class Node(HasParent):
     def __init__(self, *args, **kwargs):
         super(Node, self).__init__(*args, **kwargs)
 
     def draw_apply(self):
+        self.draw_begin()
+        
         if isinstance(self, HasChildren):
             for child in self.children:
                 child.draw_apply()
 
-        self.draw_begin()
         self.draw()
         self.draw_end()
 
@@ -113,13 +143,18 @@ class Geometry(Graphic):
         self.setup_geometry()
 
     def setup_geometry(self):
-        self.vertex_buffer, self.vertex_index_buffer = glGenBuffers(2)
+        self.vertex_buffer, self.vertex_index_buffer, self.vertex_color_buffer = glGenBuffers(3)
 
         glBindBuffer(GL_ARRAY_BUFFER, self.vertex_buffer)
         glBufferData(GL_ARRAY_BUFFER, self.vertices, GL_STATIC_DRAW)
         
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, self.vertex_index_buffer)
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, self.vertex_indices, GL_STATIC_DRAW)
+
+        self.vertex_colors = np.array(np.random.rand(len(self.vertices)), dtype=np.float32)
+            
+        glBindBuffer(GL_ARRAY_BUFFER, self.vertex_color_buffer)
+        glBufferData(GL_ARRAY_BUFFER, self.vertex_colors, GL_STATIC_DRAW)
         
 class QuadSet(Geometry):
     def __init__(self, vertices, vertex_indices, *args, **kwargs):
@@ -133,15 +168,18 @@ class QuadSet(Geometry):
         glBindBuffer(GL_ARRAY_BUFFER, self.vertex_buffer)
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, None)
 
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, self.vertex_index_buffer)
-        glDrawElements(GL_QUADS, 6, GL_UNSIGNED_SHORT, None)
+        glBindBuffer(GL_ARRAY_BUFFER, self.vertex_color_buffer)
+        glColorPointer(4, GL_FLOAT, 0, None)
 
-        glEnableVertexAttribArray(0)
-        
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, self.vertex_index_buffer)
+        glDrawElements(GL_QUADS, 24, GL_UNSIGNED_SHORT, None)
+
+        glDisableVertexAttribArray(0)
+
 class Cube(QuadSet):
     def __init__(self, size, *args, **kwargs):
         vertices, vertex_indices = self.create_geometry(size)
-        
+
         super(QuadSet, self).__init__(vertices=vertices,
                                       vertex_indices=vertex_indices,
                                       *args, **kwargs)
@@ -172,6 +210,14 @@ class TransformGroup(Group):
 
         self.matrix = matrix
 
+    def draw_begin(self):
+        glPushMatrix()
+        #glLoadMatrixf(self.matrix.values)
+        glMultMatrixf(self.matrix.values)
+
+    def draw_end(self):
+        glPopMatrix()
+
 class World(Group):
     __metaclass__ = Singleton
 
@@ -181,12 +227,12 @@ class World(Group):
 def init_world():
     world = World()
     rot_group = TransformGroup()
-    trans_group = TransformGroup()
-    cube = Cube(0.2)
+    trans_group = TransformGroup(matrix=Matrix4x4.create_translate_matrix(0, 0, -20))
+    cube = Cube(10)
 
-    trans_group.add_child(cube)
-    rot_group.add_child(trans_group)
-    world.add_child(rot_group)
+    rot_group.add_child(cube)
+    trans_group.add_child(rot_group)
+    world.add_child(trans_group)
 
 def draw_fn():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
@@ -197,7 +243,7 @@ def draw_fn():
     glutSwapBuffers()
 
 def resize_fn(width, height):
-    return
+    #return
 
     if height == 0:
         height = 1
@@ -206,11 +252,29 @@ def resize_fn(width, height):
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
     gluPerspective(45.0, float(width) / float(height), 0.1, 100.0)
+    #gluLookAt(0, 30, 30,
+    #          0, 0, 0,
+    #          0, 0, 1)
     glMatrixMode(GL_MODELVIEW)
 
 def kb_fn(key, x, y):
     if key == 27:
         sys.exit(0)
+
+xa = 0.0
+ya = 0.0
+za = 0.0
+
+def idle_fn():
+    global xa, ya, za
+    
+    World().children[0].children[0].matrix = Matrix4x4.create_rotate_matrix(xa, ya, za)
+
+    xa += 0.01
+    ya += 0.02
+    za += 0.03
+    
+    glutPostRedisplay()
 
 def init_glut(width, height):
     glutInit(sys.argv)
@@ -225,7 +289,7 @@ def init_glut(width, height):
 
     glutDisplayFunc(draw_fn)
 
-    #glutIdleFunc(draw_fn)
+    glutIdleFunc(idle_fn)
 
     glutReshapeFunc(resize_fn)
 
@@ -238,12 +302,7 @@ def init_gl(width, height):
     glEnable(GL_DEPTH_TEST)				# Enables Depth Testing
     glShadeModel(GL_SMOOTH)				# Enables Smooth Color Shading
 
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()					# Reset The Projection Matrix
-										# Calculate The Aspect Ratio Of The Window
-    gluPerspective(45.0, float(width) / float(height), 0.1, 100.0)
-
-    glMatrixMode(GL_MODELVIEW)
+    resize_fn(width, height)
 
 def main():
     w, h = 640, 480
